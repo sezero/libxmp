a60c85812b2706e80133d0eb33fe5e090e3511c7
AliceLR <petrifiedrowan@gmail.com>
2026-09-06 03:51:36 -0600

    Fix missing Protracker sample change bounds check.
    Protracker 2 samples changes could cause null dereference bugs in
    the mixer when playing a Digitrakker MDL in Protracker mode. These
    modules can contain instruments and no samples and still be valid;
    this causes mod->xxs to be NULL.

--- test-dev/test_fuzzer_play_mdl_zero_samples.c
+++ test-dev/test_fuzzer_play_mdl_zero_samples.c
@@ -1,15 +1,26 @@
 #include "test.h"
 
-/* This input caused crashes in read_event_ft2 due to a missing check
- * on subinstrument sample IDs greater than the module sample count.
+/* MDL modules without samples but with instruments can exist and are
+ * valid. Modules like this have found bugs in the FT2 and MOD player
+ * routines, so try playing a few frames with different players.
  */
 
 TEST(test_fuzzer_play_mdl_zero_samples)
 {
 	static const struct playback_sequence sequence[] =
 	{
-		{ PLAY_FRAMES,	2, 0 },
-		{ PLAY_END,	0, 0 }
+		{ PLAY_FRAMES,		8, 0 },
+		{ PLAY_SET_PLAYER_MODE,	XMP_MODE_FT2, 0 },
+		{ PLAY_FRAMES,		8, 0 },
+		{ PLAY_SET_PLAYER_MODE,	XMP_MODE_PROTRACKER, 0 },
+		{ PLAY_FRAMES,		8, 0 },
+		{ PLAY_SET_PLAYER_MODE,	XMP_MODE_ST3, 0 },
+		{ PLAY_FRAMES,		8, 0 },
+		{ PLAY_SET_PLAYER_MODE,	XMP_MODE_IT, 0 },
+		{ PLAY_FRAMES,		8, 0 },
+		{ PLAY_SET_PLAYER_MODE,	XMP_MODE_ITSMP, 0 },
+		{ PLAY_FRAMES,		8, 0 },
+		{ PLAY_END,		0, 0 }
 	};
 	compare_playback("data/f/play_mdl_zero_samples.mdl", sequence, 4000, 0, 0);
 }
